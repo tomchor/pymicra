9bf1f60d3aa7e60c93ee9a6ceb3c90442cba4c4e
tomaschor <t.chor0@mail.com>
2015-08-19 17:19:08 -0300

    Improved setup and readme

--- README.md
+++ README.md
@@ -4,8 +4,15 @@ This package is still under construction. More will come soon.
 
 ## Required Packages
 * Pandas
-* NumPy
-* setuptools (only for installation
 
 ## License
 This package is licensed under GNU General Public License V3.0 (http://choosealicense.com/licenses/gpl-3.0/)
+
+## Main Features
+A small list of things this package is designed to do:
+
+  - Automatically read and understand micrometeorological data in virtual any ASCII format (thanks to pandas).
+  - Interpolate and detrend data in the most common ways (block averages, moving averages and linear detrending).
+  - Automatically rotate the coordinates given wind data, so that the v and w averages are null.
+  - Calculate fluxes
+
--- setup.py
+++ setup.py
@@ -1,11 +1,37 @@
-from setuptools import setup
+#!/usr/bin/env python
+
+"""
+Parts of this file were taken from Pandas under the
+Apache License, Version 2.0
+"""
+
+dependencies=['Pandas', 'Numpy']
+
+try:
+	from setuptools import setup
+	_has_setuptools = True
+except ImportError:
+	# no setuptools installed
+	from distutils.core import setup
+	_has_setuptools = False
+
+if _has_setuptools:
+	extra_kwargs={'install_requires' : dependecies }
+else:
+	import pkg_resources
+	pkg_resources.require(dependencies)
+	extra_kwargs={}
+
+
 
 setup(name='pymicra',
       version='0.1',
       description='A Python Micrometeorology Analysis tool',
+      long_description=open('README.md').read(),
       url='https://github.com/tomchor/pymicra.git',
       author='Tomas Chor',
       author_email='t.chor0@gmail.com',
       license='GNU GPL V3.0',
       packages=['pymicra'],
-      zip_safe=False)
+      zip_safe=False,
+      **extra_kwargs)
