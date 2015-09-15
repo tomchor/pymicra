70d22637b9b23f6cb8c46f73441d30e7755f9987
tomas <tomaschor@gmail.com>
2015-09-15 14:59:48 -0300

    added util file

---
+++ pymicra/util.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+"""
+Author: Tomas Chor
+Date: 2015-08-07
+-------------------------
+
+This module works with micrometeorological data using pandas, numpy, datetime and several other packages
+
+-------------------------
+
+Modifications:
+CHECKLIST
+-INCLUDE SEPARATION OF FILES
+-INCLUDE QUALITY CONTROL FEATURE
+-INCLUDE MAYBE DECODIFICAION OF DATA
+
+"""
+
