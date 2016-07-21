12953132849c85a75aca1c8d2de7429483b3d245
tomchor <tomaschor@gmail.com>
2016-07-21 12:42:21 -0300

    changed docs

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -136,7 +137,7 @@ html_static_path = ['_static']
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-#html_extra_path = []
+html_extra_path = []
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
--- docs/index.rst
+++ docs/index.rst
@@ -3,13 +3,16 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to pymicra's documentation!
+Welcome to Pymicra's documentation!
 ===================================
+
 
 Contents:
 
 .. toctree::
    :maxdepth: 2
+   pymicra
+
 
 
 
