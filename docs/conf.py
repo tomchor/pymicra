b06afe6d64373924403b64e296fe3b6a38e34ea2
tomchor <tomaschor@gmail.com>
2017-09-18 13:17:50 -0700

    testing import pymicra

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,8 @@ copyfile('README.rst', '../README.rst')
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('../pymicra'))
-#import pymicra as pm
+sys.path.insert(0, os.path.abspath('../'))
+import pymicra as pm
 
 # -- General configuration ------------------------------------------------
 
