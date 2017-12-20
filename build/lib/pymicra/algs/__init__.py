7f86691020b9a05316f95ce6a5d615e96a99ecdb
tomchor <tomaschor@gmail.com>
2017-12-20 10:23:55 -0800

    cleanup

--- build/lib/pymicra/algs/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, print_function, division
-from . import units
-from . import general
-
-from .auxiliar import *
-from .general import *
-from .numeric import *
-from .units import *
--- build/lib/pymicra/micro/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Submodule that holds the specifically micrometeorological functions and variables
-"""
-from __future__ import absolute_import, print_function, division
-#from variables import *
-from .functions import *
-from .util import *
-from .scales import *
-from . import spectral
