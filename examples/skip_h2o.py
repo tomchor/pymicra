b144a8826768c0fb7b6d564ea68be4dc8ee7813f
tomchor <tomaschor@gmail.com>
2018-02-11 21:43:49 -0800

    fixed bug in preProcess with skip_h2o

--- examples/skip_h2o.py
+++ examples/skip_h2o.py
@@ -21,13 +21,14 @@ allresults=[]
 for fname in filelist:
     data, units = pm.timeSeries(fname, fileconf, parse_dates=True)
     data = pm.micro.preProcess(data, units, expand_temperature=True, rotation='2d',
-        use_means=False, rho_air_from_theta_v=True, skip_h2o=False,
+        use_means=False, rho_air_from_theta_v=True, skip_h2o=True,
         solutes=['co2'], inplace_units=True)
     ddata = data.detrend(how='linear', units=units, ignore=['theta', 'p'])
     data = data.join(ddata)
 
-    results = pm.micro.eddyCovariance(data, units, site_config=siteconf, get_turbulent_scales=True, wpl=True)
-    allresults.append(results)
+    print(data.with_units(units))
+#    results = pm.micro.eddyCovariance(data, units, site_config=siteconf, get_turbulent_scales=True, wpl=True)
+#    allresults.append(results)
 
-fluxes = pd.concat(allresults)
-print(fluxes.with_units(units))
+#fluxes = pd.concat(allresults)
+#print(fluxes.with_units(units))
